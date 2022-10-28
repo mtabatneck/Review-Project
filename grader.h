95b3cd29740281f883de2dedc37446f04f4b70de
mtabatneck <98777194+mtabatneck@users.noreply.github.com>
2022-10-28 18:45:59 -0400

    Create grader.h

---
+++ grader.h
@@ -0,0 +1,32 @@
+#ifndef GRADER_H
+#define GRADER_H
+
+
+class grader {
+    public:
+//functions------------------------------------------------------------------------------------------------------------------------
+    double lab_grader(std::vector<double> lab);
+    
+    double assignments_grader(std::vector<double> assignments);
+    
+    double project_final_grader(double project_final);
+    
+    double final_exam_grader(double final_exam);
+    
+    double project_review_grader(double project_review);
+    
+    
+};
+//---------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+#endif //GRADER_H
+
+
+
+
